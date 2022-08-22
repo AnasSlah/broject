9f44fdeac3e6f98b6d0ef8f4ce21c3bae80aca1f
AnasSlah <anasahmad2039@gmail.com>
2022-08-22 21:06:59 +0300

    lolo anas

---
+++ main.js
@@ -0,0 +1,17 @@
+let list = document.querySelectorAll(".sci li");
+let bg = document.querySelector(".socialMedia");
+list.forEach((element) => {
+  element.addEventListener("mouseenter", function (event) {
+    let color = event.target.getAttribute("data-color");
+    bg.style.backgroundColor = color;
+  });
+  element.addEventListener("mouseleave", function (event) {
+    bg.style.backgroundColor = "#fff";
+  });
+});
+VanillaTilt.init(document.querySelectorAll(".sci li a"), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  "max-glare": 1,
+});
